a3bb391dd6e08fdf25bb490f4754a3184118de62
xiaoxinbo <xiaoxinbo118@163.com>
2016-08-04 16:03:41 +0800

    runtime

--- CommonSDK/CommonSDK/CommonSDK/Category/Foundation/NSObject+RunTime.swift
+++ CommonSDK/CommonSDK/CommonSDK/Category/Foundation/NSObject+RunTime.swift
@@ -39,10 +39,86 @@ public extension NSObject {
     *  @param object 被关联对象
     *  @param key   key
     */
-    func rtm_associateStrongNonatomicObject(obj:AnyObject!, key aKey:String!) {
+    func rtm_associateStrongNonatomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+    static func rtm_associateStrongNonatomicObject(obj:AnyObject?, key aKey:String!) {
         objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
     
+    /*!
+    *  @brief  关联策略：strong, atomic
+    *
+    *  @param object 被关联对象
+    *  @param key   key
+    */
+    func rtm_associateStrongAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+    static func rtm_associateStrongAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
     
+    /*!
+    *  @brief  关联策略：copy, nonatomic
+    *
+    *  @param object 被关联对象
+    *  @param key   key
+    */
+    func rtm_associateCopyNonatomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC)
+    }
+    static func rtm_associateCopyNonatomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC)
+    }
+
+    /*!
+    *  @brief  关联策略：copy, atomic
+    *
+    *  @param object 被关联对象
+    *  @param key   key
+    */
+    func rtm_associateCopyAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY)
+    }
+    static func rtm_associateCopyAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY)
+    }
     
+    /*!
+    *  @brief  关联策略：weak
+    *
+    *  @param object 被关联对象
+    *  @param key   key
+    */
+    func rtm_associateWeakAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+    }
+    static func rtm_associateWeakAtomicObject(obj:AnyObject?, key aKey:String!) {
+        objc_setAssociatedObject(self, aKey, obj, objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+    }
+    
+    /*!
+    *  @brief  获取关联对象
+    *
+    *  @param key keu
+    *
+    *  @return 被关联对象的value
+    */
+    func rtm_associatedObjectForKey(key:String) -> AnyObject! {
+        return objc_getAssociatedObject(self, key);
+    }
+    static func rtm_associatedObjectForKey(key:String) -> AnyObject! {
+        return objc_getAssociatedObject(self, key);
+    }
+    
+    /*!
+    *  @brief  移除所有关联对象
+    */
+    func rtm_removeAllAssociatedObjects() {
+        objc_removeAssociatedObjects(self);
+    }
+    static func rtm_removeAllAssociatedObjects() {
+        objc_removeAssociatedObjects(self);
+    }
 }
