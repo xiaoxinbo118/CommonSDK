421764f82f9a50695bb2ba2b0d9d9f435689d343
xiaoxinbo <xiaoxinbo118@163.com>
2016-08-09 16:40:06 +0800

    kvo

---
+++ CommonSDK/CommonSDK/CommonSDK/Category/Foundation/NSObject+KVO.swift
@@ -0,0 +1,22 @@
+//
+//  NSObject+KVO.swift
+//  CommonSDK
+//
+//  Created by 肖信波 on 16/8/9.
+//  Copyright © 2016年 fengqu. All rights reserved.
+//
+
+import UIKit
+
+private class CMNSafeKVO : NSObject {
+    var keyPath : String?;
+    
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+    }
+}
+
+public extension NSObject {
+    
+}
